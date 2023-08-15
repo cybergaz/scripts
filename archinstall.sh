afb96533191fd2d7bcf7d7894cfaa1538e830cb1
cybergaz <kkanttechy@gmail.com>
2023-08-16 01:43:27 +0530

    ....

--- archinstall.sh
+++ archinstall.sh
@@ -1,7 +1,9 @@
 #part1
 clear
 echo "Welcome to Gaz's arch installer script"
+sleep 1
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 10/" /etc/pacman.conf
+reflector --country "Austrelia,Germany,India,Taiwan,Singapore" --save /etc/pacman.d/mirrorlist
 pacman --noconfirm -Sy archlinux-keyring
 loadkeys us
 timedatectl set-ntp true
@@ -24,11 +26,11 @@ genfstab -U /mnt >>/mnt/etc/fstab
 sed '1,/^#part2$/d' $(basename $0) >/mnt/arch_install2.sh
 chmod +x /mnt/arch_install2.sh
 arch-chroot /mnt ./arch_install2.sh
-exit
 
 #part2
 clear
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 10/" /etc/pacman.conf
+reflector --country "Austrelia,Germany,India,Taiwan,Singapore" --save /etc/pacman.d/mirrorlist
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
@@ -46,13 +48,13 @@ echo "127.0.1.1       $hostname.localdomain $hostname" >>/etc/hosts
 mkinitcpio -P
 passwd
 
-pacman --noconfirm -S grub efibootmgr os-prober
+pacman --noconfirm -Sy grub efibootmgr os-prober
 lsblk
 echo "Enter EFI partition: "
 read efipartition
 mkdir /boot/efi
 mount $efipartition /boot/efi
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux
 sed -i 's/quiet/pci=noaer/g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/g' /etc/default/grub
 sed -i 's/#GRUB_DISABLE_OS_PROBER=true/GRUB_DISABLE_OS_PROBER=false/g' /etc/default/grub
@@ -60,19 +62,19 @@ mount $efipartition /boot/efi/
 os-prober
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S --noconfirm networkmanager wpa_supplicant rsync zip unzip unrar fzf
+pacman -Sy --noconfirm networkmanager wpa_supplicant rsync zip unzip unrar fzf git
 
 systemctl enable NetworkManager.service
 
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
 echo "Enter Username: "
 read username
-useradd -m -G wheel -s /bin/zsh $username
+useradd -m -G wheel -s /bin/sh $username
 passwd $username
 
-sed -i 's/#HandlePowerKey=poweroff/HandlePowerKey=ignore' /etc/systemd/logind.conf
-sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore' /etc/systemd/logind.conf
-sed -i 's/##HandleLidSwitchExternalPower=suspend/HandleLidSwitchExternalPower=ignore' /etc/systemd/logind.conf
+sed -i 's/#HandlePowerKey=poweroff/HandlePowerKey=ignore/' /etc/systemd/logind.conf
+sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+sed -i 's/##HandleLidSwitchExternalPower=suspend/HandleLidSwitchExternalPower=ignore/' /etc/systemd/logind.conf
 
 echo "Pre-Installation Finish Reboot now"
 
