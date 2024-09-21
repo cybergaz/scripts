a01085c598dc0c8a0f2240bfd84b8d3e087bffea
cybergaz <kkanttechy@gmail.com>
2024-09-21 21:08:42 +0530

    update: niri workspace script

--- niri/next_empty_workspace.sh
+++ niri/next_empty_workspace.sh
@@ -1,5 +1,8 @@
-workspaces=$(niri msg --json workspaces)
+# workspaces=$(niri msg --json workspaces)
+# echo $workspaces
+#
+# last_entry=$(echo "$workspaces" | jq '.[-1]')
+# echo $last_entry
 
-last_entry=$(echo "$workspaces" | jq '.[-1]')
-
-niri msg action focus-workspace $(echo "$last_entry" | jq -r '.idx')
+# niri msg action focus-workspace $(echo "$last_entry" | jq -r '.idx')
+niri msg action focus-workspace $(niri msg workspaces | tail -n 1 | tr -d " \t")
